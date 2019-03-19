01a64dc5541eed7bb47d102ff58fa3d968c1688e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-19 16:30:13 +0000

    Automatically backed up by Learn

--- db/migrate/20160114135651_add_recipe_id_to_ingredients.rb
+++ db/migrate/20160114135651_add_recipe_id_to_ingredients.rb
@@ -1,5 +1,5 @@
 class AddRecipeIdToIngredients < ActiveRecord::Migration
   def change
-    add_column :ingredients, :recipe_id, :integer
+    add_reference :ingredients, :recipe, index: true, foreign_key: true
   end
 end
