57c6e560a5a6d32d97d1d8f65a86c7772f9cb58e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-19 16:34:35 +0000

    Automatically backed up by Learn

--- spec/features/recipe_form_spec.rb
+++ spec/features/recipe_form_spec.rb
@@ -13,8 +13,8 @@ describe "the recipe form", :type => :feature do
     visit '/recipes/new'
     fill_in :recipe_ingredients_attributes_0_quantity, with: '1 cup'
     fill_in :recipe_ingredients_attributes_0_name, with: 'sugar'
-    # fill_in :recipe_ingredients_attributes_1_quantity, with: '1 tablespoon'
-    # fill_in :recipe_ingredients_attributes_1_name, with: 'vanilla'
+    fill_in :recipe_ingredients_attributes_1_quantity, with: '1 tablespoon'
+    fill_in :recipe_ingredients_attributes_1_name, with: 'vanilla'
     find('input[name="commit"]').click
     expect(Recipe.last.ingredients.map(&:quantity)).to eq ['1 cup', '1 tablespoon']
     expect(Recipe.last.ingredients.map(&:name)).to eq ['sugar', 'vanilla']
