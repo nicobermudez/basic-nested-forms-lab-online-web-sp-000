90aedde1ee9ff31b86af917382241fbab75b437d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-19 16:28:45 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,9 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    if @recipe.ingredients.last.try(:name)
+      @recipe.ingredients.build
+    end
   end
 
   def index
@@ -9,7 +12,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build
+    2.times { @recipe.ingredients.build }
   end
 
   def create
@@ -17,14 +20,15 @@ class RecipesController < ApplicationController
     redirect_to recipe
   end
 
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title,
-    ingredients_attributes: [
-      :id,
-      :name,
-      :quantity
-      ])
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
